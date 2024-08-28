f2b87a8591ae3b0c252fec1d6ed99f8ac3d5d303
SirrBarnes <torressergio1357@gmail.com>
2024-08-28 17:15:55 -0400

    started coding for pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/About',
+        element: <AboutPage />,
+      },
+      {
+        path: '/Contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/Portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: '/Resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router = { router } />
 )
