880b8d8b0b11cea405bbdcccfa4ece76780a3137
SirrBarnes <torressergio1357@gmail.com>
2024-09-06 18:44:44 -0400

    updated changes to site

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# React + Vite
+# React-Portfolio
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Description
 
-Currently, two official plugins are available:
+The application that you built for this module will help you know where to start when building your portfolio. You’ll use Create Vite just as you did with your module project, but you should be aware of a few structural differences in the component hierarchy. Your portfolio should include the following:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- A single Header component that appears on multiple pages
+
+- A single Navigation component within the header that will be used to render the different sections of your portfolio
+
+- A single Project component that will be used multiple times in the Portfolio section
+
+- A single Footer component that appears on multiple pages
+
+
+## Installation
+
+To install simply download from the github repository, open a terminal at the root of the file structure, run "npm i" and then "npm run start". The application should open up at "localhost: 3000"
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -4,43 +4,43 @@ const projectCards = [
     {
         id: 1,
         title: 'Animal Shelter Website',
-        description: 'project 1',
-        image: '',
+        description: 'This app is a mock animal shelter website that enables the user to view, checkout and adopt pets as if it were from an actual adoption site. ',
+        image: "src/images/AnimalPage.PNG",
         link: 'https://github.com/SirrBarnes/Animal-Shelter-Project1'
     },
     {
         id: 2,
         title: 'Employee Tracker',
-        description: 'project 1',
-        image: '',
+        description: "This app is a command-line application made from scratch to manage a company's employee database, using Node.js, Inquirer, and PostgreSQL.",
+        image: 'src/images/Employee-Tracker.png',
         link: 'https://github.com/SirrBarnes/Employee-Tracker'
     },
     {
         id: 3,
         title: 'Vehicle Builder',
-        description: 'project 1',
-        image: '',
+        description: "This app prompts the user to either create a new vehicle or select an existing vehicle. After going through the creation or selection process, the user is able to perform certain actions with the selected vehicle. The user is returned to the actions menu after each action until they decide to exit the application",
+        image: 'src/images/Vehicle-Builder.png',
         link: 'https://github.com/SirrBarnes/Vehicle-Builder'
     },
     {
         id: 4,
-        title: 'Personal Blog Mockup',
-        description: 'project 1',
-        image: '',
+        title: 'Personal Blog',
+        description: "This app includes building a content form, dynamically rendering blog posts, and implementing a light/dark mode toggle",
+        image: 'src/images/Personal-Blog.png',
         link: 'https://github.com/SirrBarnes/Personal-Blog-Mockup'
     },
     {
         id: 5,
         title: 'Employee Payroll Tracker',
-        description: 'project 1',
-        image: '',
+        description: 'This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.',
+        image: 'src/images/Employee-Payroll-Tracker.png',
         link: 'https://github.com/SirrBarnes/Employee-Payroll-Tracker'
     },
     {
         id: 6,
         title: 'Professional README Generator',
-        description: 'project 1',
-        image: '',
+        description: "This app is designed to create a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package",
+        image: 'src/images/ReadME-Generator.png',
         link: 'https://github.com/SirrBarnes/Professional-README-Generator'
     }
 ];
@@ -57,7 +57,7 @@ const PortfolioPage = () => {
                             <h3>{project.title}</h3>
                             <p>{project.description}</p>
                         </div>
-                        <img src='' alt='' className='' />
+                        <img src={project.image} alt={project.title} className='cardImage' />
                     </div>
                 ))}
             </div>
